61a4fd21fb610cd2f77bf856fcbf390b5106d15a
Saloni Dattani <31684905+saloni-nd@users.noreply.github.com>
2023-09-24 10:27:58 +0100

    Create change-mx-lexis-plot.R

---
+++ period-vs-cohort/change-mx-lexis-plot.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(scales)
+library(viridis)
+library(ggrepel)
+library(data.table)
+
+# Data source: Human Mortality Database, on mortality.org
+# Download and save to folder below, as `Mx_1x1_[country].txt`
+file_path <- ""
+
+countries <- c("FRATNP")
+mortality <- list()
+
+# Import data
+for (country in countries) {
+  
+  # Import and rename cols
+  mortality[[country]] <- read_table(paste0(file_path, "Mx_1x1_", country, ".txt"), skip=2)
+  colnames(mortality[[country]]) <- c("Year", "Age", "Female", "Male", "Total")
+  
+  mortality[[country]] <- mortality[[country]] %>%
+    mutate(Country = country)
+  
+}
+
+
+# Join into single df
+mortality <- do.call(rbind.data.frame, mortality)
+
+# Reformat
+mortality$Age <- as.integer(mortality$Age)
+mortality$Female <- as.numeric(mortality$Female)
+mortality$Male <- as.numeric(mortality$Male)
+mortality$Total <- as.numeric(mortality$Total)
+mortality$Country <- as.factor(mortality$Country)
+
+# Gather
+mortality <- mortality %>%
+              gather(key = Sex, value = Mx, Female:Total)
+
+# Create vars for log10 mortality rate and ln mortality rate
+mortality <- mortality %>%
+  mutate(
+    birth_year = Year - Age
+  ) %>%
+  mutate(
+    log10_cmr  = log(Mx, base = 10),
+    ln_mr      = log(Mx, base = exp(1))
+  )
+
+mortality <- mortality %>%
+  arrange(Country, Age, Sex) %>%
+  group_by(Country, Sex) %>%
+  mutate(ch_mr = Mx - lag(Mx)) %>%  # must be done separately by group
+  ungroup()
+
+mortality <- mortality %>%
+  group_by(Sex, Age) %>%
+  mutate(change_log10_cmr = log10_cmr - lag(log10_cmr)) %>%
+  ungroup()
+
+
