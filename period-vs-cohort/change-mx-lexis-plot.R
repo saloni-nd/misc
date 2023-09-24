9968a84ae852ff74c608b854fdd7076a931b76be
Saloni Dattani <31684905+saloni-nd@users.noreply.github.com>
2023-09-24 11:18:59 +0100

    Update change-mx-lexis-plot.R

--- period-vs-cohort/change-mx-lexis-plot.R
+++ period-vs-cohort/change-mx-lexis-plot.R
@@ -3,17 +3,14 @@ library(scales)
 library(viridis)
 library(ggrepel)
 library(data.table)
+library(RColorBrewer)
+library(lme4)
 
 # Data source: Human Mortality Database, on mortality.org
 # Download and save to folder below, as `Mx_1x1_[country].txt`
-
-# Code source: Methods for disentangling period and cohort changes in mortality risk over the twentieth century: comparing graphical and modelling approaches (2022). Phil Mike Jones, Jon Minton & Andrew Bell.
-# https://link.springer.com/article/10.1007/s11135-022-01498-3
-# Code: https://zenodo.org/record/6866402
-
 file_path <- ""
 
-countries <- c("FRATNP")
+countries <- c("FRATNP", "ITA")
 mortality <- list()
 
 # Import data
@@ -43,8 +40,58 @@ mortality$Country <- as.factor(mortality$Country)
 mortality <- mortality %>%
               gather(key = Sex, value = Mx, Female:Total)
 
+
+### Try using populations and deaths instead
+
+population <- list()
+deaths <- list()
+
+# Import data
+for (country in countries) {
+  
+  # Import and rename cols
+  population[[country]] <- read_table(paste0(file_path, "Population_", country, ".txt"), skip=2)
+  colnames(population[[country]]) <- c("Year", "Age", "Female", "Male", "Total")
+  
+  population[[country]] <- population[[country]] %>%
+    mutate(Country = country)
+  
+}
+
+# Import data
+for (country in countries) {
+  
+  # Import and rename cols
+  deaths[[country]] <- read_table(paste0(file_path, "Deaths_", country, ".txt"), skip=2)
+  colnames(deaths[[country]]) <- c("Year", "Age", "Female", "Male", "Total")
+  
+  deaths[[country]] <- deaths[[country]] %>%
+    mutate(Country = country)
+  
+}
+
+# Join into single df
+population <- do.call(rbind.data.frame, population)
+deaths <- do.call(rbind.data.frame, deaths)
+
+# Gather
+population <- population %>%
+  gather(key = Sex, value = Population, Female:Total)
+
+deaths <- deaths %>%
+  gather(key = Sex, value = Deaths, Female:Total)
+
+joined <- merge(population, deaths, by=c("Country", "Year", "Age", "Sex"))
+joined <- joined %>% mutate(Mx = Population / Deaths)
+
+joined$Year <- as.numeric(joined$Year)
+joined$Age <- as.numeric(joined$Age)
+
+
+######## Use either `mortality` or `joined` here
+
 # Create vars for log10 mortality rate and ln mortality rate
-mortality <- mortality %>%
+mort_hmd <- mortality %>%
   mutate(
     birth_year = Year - Age
   ) %>%
@@ -53,15 +100,63 @@ mortality <- mortality %>%
     ln_mr      = log(Mx, base = exp(1))
   )
 
-mortality <- mortality %>%
+mort_hmd <- mort_hmd %>%
   arrange(Country, Age, Sex) %>%
   group_by(Country, Sex) %>%
   mutate(ch_mr = Mx - lag(Mx)) %>%  # must be done separately by group
   ungroup()
 
-mortality <- mortality %>%
+mort_hmd <- mort_hmd %>%
+  arrange(Country, Age, Sex) %>%
   group_by(Sex, Age) %>%
   mutate(change_log10_cmr = log10_cmr - lag(log10_cmr)) %>%
   ungroup()
+
+mort_hmd <- mort_hmd %>%
+  mutate(
+    change_log10_cmr = ifelse(change_log10_cmr < -0.1, -0.1, change_log10_cmr),
+    change_log10_cmr = ifelse(change_log10_cmr > 0.1, 0.1, change_log10_cmr)
+  )
+
+# Data above age 90 is pretty bad
+mort_hmd <- mort_hmd %>%
+  filter(Age  < 91,
+         Year > 1899)
+
+
+cores = parallel::detectCores() - 1L
+
+
+# Lexis surface (contour) plots
+parallel::mclapply(as.list(unique(mort_hmd$Country)), function(Country) {
+  
+  mort_hmd <- mort_hmd[mort_hmd$Country == Country, ]
+  
+  mort_plot <- mort_hmd %>%
+    lattice::levelplot(
+      change_log10_cmr ~ Year * Age | Sex,
+      data = .,
+      par.strip.text = list(cex = 1.4, fontface = "bold"),
+      xlab = list(label = "Year", cex = 1.4),
+      ylab = list(label = "Age in years", cex = 1.4),
+      par.settings = list(strip.background = list(col = "lightgrey")),
+      scales = list(
+        x = list(cex = 1.2, at = seq(1900, 2010, by = 10), rot = 90),
+        y = list(cex = 1.2, at = seq(0, 90, by = 10)),
+        alternating = TRUE
+      ),
+      aspect = "iso",
+      col.regions = rev(colorRampPalette(brewer.pal(6, "Spectral"))(200)),
+      at = seq(-0.1, 0.1, by = 0.025)
+    )
+  
+  png(filename = paste0(file_path, "mort_", Country, ".png"),
+      width = 420, height = 148, units = "mm", res = 300)
+  print(mort_plot)
+  dev.off()
+  
+},
+mc.cores = cores
+)
 
 
