ad394cb6f8d679271e029c1ca73352eebe4f533b
Saloni Dattani <31684905+saloni-nd@users.noreply.github.com>
2024-12-25 00:11:36 +0000

    Added types and updated colors

--- antibiotics-timeline/antibiotics-us-approvals/antibiotics-us-approvals.R
+++ antibiotics-timeline/antibiotics-us-approvals/antibiotics-us-approvals.R
@@ -12,11 +12,11 @@ file_path <- ""
 
 ab_drugs <- read_excel(paste0(file_path, "Antibiotics_approvals_US.xlsx"),
                        sheet=1,
-                       col_types=c("text", "text", "numeric", 
+                       col_types=c("text", "text", "text", "numeric", 
                                    "numeric", "text", 
                                    "text", "text"))
 
-colnames(ab_drugs) <- c("Class", "Drug", "Year", "MCS_cluster", "ISO_type", "Molecular_weight", "Source")
+colnames(ab_drugs) <- c("Type", "Class", "Drug", "Year", "MCS_cluster", "ISO_type", "Molecular_weight", "Source")
 
 # Arrange by year
 ab_drugs <- ab_drugs %>%
@@ -31,14 +31,19 @@ ab_drugs$First <- +(!duplicated(ab_drugs$Class))
 
 ab_drugs <- as_tibble(ab_drugs)
 
+# Select colors
+group.colors <- c("Beta lactams" = "#38AABA", 
+                  "MLS family" = "#FDE12D", 
+                  "Other antibiotics" ="#9649CB")
 
 ggplot(data=ab_drugs, aes(x=Year, 
                           y=id, 
                           label=Class)) +
-  geom_point(fill="#38AABA", color="black", size=2.5, pch=21, stroke=0.8) +
+  geom_point(aes(fill=Type), color="black", size=2.5, pch=21, stroke=0.8) +
   # Show names
   geom_text(hjust=1, nudge_x=-2.5, data=filter(ab_drugs,First==1), size=3.5) + 
   theme_classic() +
+  scale_fill_manual(values=group.colors) +
   theme(text = element_text(family = "Lato"), # Set Lato for all text elements
         axis.text.y=element_blank(),
         axis.text.x = element_text(size = 12), # Adjust x-axis label size
@@ -46,6 +51,8 @@ ggplot(data=ab_drugs, aes(x=Year,
         plot.title = element_text(size = 20),
         plot.subtitle = element_text(size = 12), # Set subtitle size
         plot.caption = element_text(size = 12),  # Set caption size
+        legend.title = element_text(size = 14),  # Adjust legend title size
+        legend.text = element_text(size = 12),   # Adjust legend categories size
         axis.title.x.top = element_text(vjust = 1.5), # Adjust x-axis title position at the top
         axis.text.x.top = element_text(vjust = 1.5),  # Adjust x-axis text position at the top
         axis.ticks.x.top = element_line(),           # Add ticks to the top x-axis
@@ -54,6 +61,7 @@ ggplot(data=ab_drugs, aes(x=Year,
        subtitle="The year when each antibiotic drug class was first approved in the United States.\nAdditional dots on each row show subsequent antibiotics of the same class.",
        x="",
        y="",
+       fill="Type",
        caption="Sources: Coussens et al. (2018)") +
   coord_cartesian(xlim=c(1930,2024)) +
   scale_y_reverse() + # Flip y-axis
